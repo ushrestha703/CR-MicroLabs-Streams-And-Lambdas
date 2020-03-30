34134eba1c62a59a6d92a001d0f4ef60164032fb
Ujjwal Shrestha <ushrestha@zipcodes-MBP.fios-router.home>
2020-03-30 14:46:39 -0400

    done.

--- src/main/java/com/zipcodewilmington/streams/StreamFilter.java
+++ src/main/java/com/zipcodewilmington/streams/StreamFilter.java
@@ -5,6 +5,7 @@ import com.zipcodewilmington.streams.anthropoid.PersonFactory;
 import com.zipcodewilmington.streams.tools.RandomUtils;
 import com.zipcodewilmington.streams.tools.StringUtils;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -15,12 +16,13 @@ import java.util.stream.Stream;
 public class StreamFilter {
     private final Stream<Person> personStream;
     public final String startingCharacter;
+    int limit = 100;
 
     /**
      * No arg constructor
      */ //TODO - construct person stream of 100 person objects; startingCharacter is a random capital letter
     public StreamFilter() {
-        this(Stream.empty(), null);
+        this(new PersonFactory().createPersonStream(100), RandomUtils.createCharacter('A', 'Z'));
     }
 
     /**
@@ -28,7 +30,8 @@ public class StreamFilter {
      * @param startingCharacter - character to filter by
      */ //TODO
     public StreamFilter(Person[] people, Character startingCharacter) {
-        this(Stream.empty(), null);
+
+        this(Arrays.stream(people),startingCharacter);
     }
 
     /**
@@ -36,7 +39,8 @@ public class StreamFilter {
      * @param startingCharacter - character to filter by
      */ //TODO
     public StreamFilter(List<Person> people, Character startingCharacter) {
-        this(Stream.empty(), null);
+
+        this(people.stream(), startingCharacter);
     }
 
 
@@ -55,7 +59,10 @@ public class StreamFilter {
      * @return a list of person object whose name starts with `this.startingCharacter`
      */ //TODO
     public List<Person> toListMultiLine() {
-        return null;
+
+        return personStream.filter(p -> {
+            return p.getName().equals(this.startingCharacter);
+        }).collect(Collectors.toList());
     }
 
 
@@ -64,7 +71,10 @@ public class StreamFilter {
      * @return a list of person objects whose name starts with `this.startingCharacter`
      */ //TODO
     public List<Person> toListOneLine() {
-        return null;
+
+        return personStream
+                .filter(p -> p.getName().equals(this.startingCharacter))
+                .collect(Collectors.toList());
     }
 
 
@@ -73,7 +83,9 @@ public class StreamFilter {
      * @return an array of person object whose name starts with `this.startingCharacter`
      */ //TODO
     public Person[] toArrayOneLine() {
-        return null;
+        return personStream
+                .filter(p -> p.getName().equals(this.startingCharacter))
+                .toArray(Person[]::new);
     }
 
 
@@ -82,7 +94,9 @@ public class StreamFilter {
      * @return an array of person object whose name starts with `this.startingCharacter`
      */ //TODO
     public Person[] toArrayMultiLine() {
-        return null;
+        return personStream.filter(p -> {
+            return p.getName().equals(this.startingCharacter);
+        }).toArray(Person[]::new);
     }
 
 }
--- src/main/java/com/zipcodewilmington/streams/StreamMap.java
+++ src/main/java/com/zipcodewilmington/streams/StreamMap.java
@@ -18,7 +18,8 @@ public class StreamMap {
      * @return - a Stream of single characters
      */ //TODO
     public static Stream<String> letters(String someWord) {
-        return null;
+
+        return Stream.of(someWord.toCharArray()).map(Object::toString);
     }
 
     /**
@@ -26,7 +27,8 @@ public class StreamMap {
      * @return - a Stream of several Streams of single characters
      */ //TODO
     public static Stream<Stream<String>> wordsMap(String... someWords) {
-        return null;
+
+        return Arrays.stream(someWords).map(StreamMap::letters);
     }
 
     /**
@@ -34,6 +36,6 @@ public class StreamMap {
      * @return - a Stream of several Streams of single characters
      */ //TODO
     public static Stream<String> wordsFlatMap(String... stringArray) {
-        return null;
+        return Arrays.stream(stringArray).flatMap(someWord -> letters(someWord));
     }
 }
